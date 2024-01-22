394933ff52abbd86c5af687e26e21adb42830279
Andry Ariadi <andryariadi23@gmail.com>
2024-01-23 06:25:16 +0700

    styling footer comp

--- i-blog/src/components/footer/Footer.jsx
+++ i-blog/src/components/footer/Footer.jsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import styles from "./footer.module.css";
+
 export default function Footer() {
-  return <>Footer Component</>;
+  return (
+    <>
+      <div>
+        <div className={styles.container}>
+          <div className={styles.logo}>
+            <Image src="/icons/footer.svg" alt="logo" width={60} height={60} className="logo" />
+          </div>
+          <div className={styles.text}>AndryAriadi creative thoughts agency © All rights reserved.</div>
+        </div>
+      </div>
+    </>
+  );
 }
