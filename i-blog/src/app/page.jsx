bca06ee1429dfdb0b510b9c5a5483e487e6df47e
Andry Ariadi <andryariadi23@gmail.com>
2024-01-23 06:04:46 +0700

    styling home page

--- i-blog/src/app/page.jsx
+++ i-blog/src/app/page.jsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 export default function Home() {
-  return <div>Hello Bre...</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+          <p className={styles.desc}> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis adipisci minima reiciendis a autem assumenda dolore.</p>
+          <div className={styles.btnContainer}>
+            <button className={styles.btn}>Learn More</button>
+            <button className={styles.btn}>Contact</button>
+          </div>
+          <div className={styles.brandContainer}>
+            <Image src="/images/brands.png" alt="brands" fill className={styles.brandImg} />
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="/images/hero.gif" alt="hero" fill className={styles.heroImg} />
+        </div>
+      </div>
+    </>
+  );
 }
--- i-blog/src/components/navbar/Navbar.jsx
+++ i-blog/src/components/navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import NavLinks from "./navLinks/NavLink";
 import styles from "./navbar.module.css";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.logo}>
+        <Link href="/" className={styles.logo}>
           <Image src="/images/logo.svg" alt="logo" width={140} height={140} className="logo" />
-        </div>
+        </Link>
         <div>
           <NavLinks />
         </div>
