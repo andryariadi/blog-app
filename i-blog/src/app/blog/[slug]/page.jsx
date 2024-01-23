e51eb3936265ab51f7ac015b20eea394a2903f2a
Andry Ariadi <andryariadi23@gmail.com>
2024-01-23 22:48:18 +0700

    styling detail post

--- i-blog/src/app/blog/[slug]/page.jsx
+++ i-blog/src/app/blog/[slug]/page.jsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import styles from "./slug.module.css";
+
 export default function PostDetailPage() {
   return (
-    <div>
-      <h1>Post Detail Page</h1>
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image src="https://images.pexels.com/photos/9595972/pexels-photo-9595972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="postdetail" fill className={styles.img} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>Title</h1>
+          <div className={styles.detail}>
+            <Image src="https://images.pexels.com/photos/9595972/pexels-photo-9595972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={50} height={50} className={styles.avatar} />
+            <div className={styles.detailText}>
+              <span className={styles.detailTitle}>Author</span>
+              <span className={styles.detailValue}>Andry Ariadi</span>
+            </div>
+            <div className={styles.detailText}>
+              <span className={styles.detailTitle}>Published</span>
+              <span className={styles.detailValue}>01.01.2024</span>
+            </div>
+          </div>
+          <div className={styles.content}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quo iste incidunt dolor ipsa rerum, sit accusamus dignissimos magnam cupiditate voluptas! Saepe maxime autem nemo earum quo aliquid? Doloribus, commodi.
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
