775aa2813c9b988d2bf1abafddec47e2ae7e66e6
Andry Ariadi <andryariadi23@gmail.com>
2024-01-23 13:38:22 +0700

    styling count comp

--- i-blog/src/components/count/Count.jsx
+++ i-blog/src/components/count/Count.jsx
@@ -2,12 +2,37 @@
 
 import styles from "./count.module.css";
 import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 export default function Count() {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
   return (
     <>
-      <div className={styles.container}>
-        <CountUp start={0} end={100} duration={3} />
+      <div className={styles.container} ref={ref}>
+        {inView && (
+          <>
+            <div className={styles.countContainer}>
+              <div className={styles.count}>
+                <CountUp start={0} end={10} duration={3} /> K+
+              </div>
+              <p className={styles.title}>Year of Experiance</p>
+            </div>
+            <div className={styles.countContainer}>
+              <div className={styles.count}>
+                <CountUp start={0} end={243} duration={3} /> K+
+              </div>
+              <p className={styles.title}>People reached</p>
+            </div>
+            <div className={styles.countContainer}>
+              <div className={styles.count}>
+                <CountUp start={0} end={5} duration={3} /> K+
+              </div>
+              <p className={styles.title}>Services and plugins</p>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
