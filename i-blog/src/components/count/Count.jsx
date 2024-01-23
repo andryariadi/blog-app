42cbb695464491555e2284b75984bb386f345b87
Andry Ariadi <andryariadi23@gmail.com>
2024-01-23 13:17:37 +0700

    install react intersection

--- i-blog/src/components/count/Count.jsx
+++ i-blog/src/components/count/Count.jsx
@@ -1,9 +1,14 @@
+"use client";
+
 import styles from "./count.module.css";
+import CountUp from "react-countup";
 
 export default function Count() {
   return (
     <>
-      <div>Count</div>
+      <div className={styles.container}>
+        <CountUp start={0} end={100} duration={3} />
+      </div>
     </>
   );
 }
