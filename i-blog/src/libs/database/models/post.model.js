db0a9742d643b59571a29dc655e61fdbdc794a11
Andry Ariadi <andryariadi23@gmail.com>
2024-01-24 23:00:43 +0700

    add post model

---
+++ i-blog/src/libs/database/models/post.model.js
@@ -0,0 +1,31 @@
+import { Schema, model, models } from "mongoose";
+
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = models.Post || model("Post", PostSchema);
+
+export default Post;
