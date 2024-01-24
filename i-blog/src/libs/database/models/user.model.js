030843daf1a6cc6b2415f29dbd3a1b022a284a41
Andry Ariadi <andryariadi23@gmail.com>
2024-01-24 22:55:14 +0700

    add user models

---
+++ i-blog/src/libs/database/models/user.model.js
@@ -0,0 +1,36 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    imgUrl: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
