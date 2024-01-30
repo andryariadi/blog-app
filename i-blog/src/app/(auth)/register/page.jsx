115989fcb8b7ef3788e42004d8601b1eddeb8fa6
Andry Ariadi <andryariadi23@gmail.com>
2024-01-30 14:36:33 +0700

    styling register page

--- i-blog/src/app/(auth)/register/page.jsx
+++ i-blog/src/app/(auth)/register/page.jsx
@@ -1,7 +1,20 @@
+import { register } from "@/libs/database/actions/action";
+import styles from "./register.module.css";
+
 export default function RegisterPage() {
   return (
     <>
-      <div>Register Page</div>
+      <div className={styles.container}>
+        <div className={styles.formContainer}>
+          <form action={register} className={styles.form}>
+            <input type="text" placeholder="username" name="username" />
+            <input type="email" placeholder="email" name="email" />
+            <input type="password" placeholder="password" name="password" />
+            <input type="text" placeholder="password again" name="passwordRepeat" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
--- i-blog/src/libs/database/actions/action.js
+++ i-blog/src/libs/database/actions/action.js
@@ -1,6 +1,8 @@
 "use server";
 
 import { signIn, signOut } from "@/libs/auth/auth";
+import { connectToDB } from "..";
+import User from "../models/user.model";
 
 export const handleGithubLogin = async () => {
   await signIn("github");
@@ -8,4 +10,31 @@ export const handleGithubLogin = async () => {
 
 export const handleLogout = async () => {
   await signOut();
+};
+
+export const register = async (formData) => {
+  const { username, email, password, passwordRepeat, imgUrl } = Object.fromEntries(formData);
+
+  if (password !== passwordRepeat) return "Passwords do not match";
+
+  try {
+    connectToDB();
+
+    const user = await User.findOne({ email });
+
+    if (user) return "User already exists";
+
+    const newUser = new User({
+      username,
+      email,
+      password,
+      imgUrl,
+    });
+
+    await newUser.save();
+    console.log("User created to DB");
+  } catch (error) {
+    console.log(error);
+    return { error: "Failed to register!" };
+  }
 };
