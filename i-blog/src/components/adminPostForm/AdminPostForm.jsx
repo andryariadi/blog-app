ba141c24481385009e757f9b2141c14c6d4e2c80
Andry Ariadi <andryariadi23@gmail.com>
2024-02-03 14:37:06 +0700

    styling post form

--- i-blog/src/components/adminPostForm/AdminPostForm.jsx
+++ i-blog/src/components/adminPostForm/AdminPostForm.jsx
@@ -8,7 +8,7 @@ export default function AdminPostForm({ userId }) {
   const [state, formAction] = useFormState(addPost, undefined);
   return (
     <>
-      <form action={formAction} className={styles.form}>
+      <form action={formAction} className={styles.container}>
         <h1>Add New Post</h1>
         <input type="hidden" name="userId" value={userId} />
         <input type="text" placeholder="Title" name="title" />
