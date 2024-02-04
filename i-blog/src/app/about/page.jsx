de6cdcf0daef87f769d1d1888cfbf26247f88a6c
Andry Ariadi <andryariadi23@gmail.com>
2024-02-04 22:12:37 +0700

    add about comp

--- i-blog/src/app/about/page.jsx
+++ i-blog/src/app/about/page.jsx
@@ -1,41 +1,14 @@
-"use client";
+import About from "@/components/about/About";
 
-import Image from "next/image";
-import styles from "./about.module.css";
-import Count from "@/components/count/Count";
-
-import { motion } from "framer-motion";
-import { fadeIn } from "../../libs/motions/variants";
-
-// export const metadata = {
-//   title: "Next.js 14 iBlog About Page",
-//   description: "andry ariadi blog about description",
-// };
+export const metadata = {
+  title: "About",
+  description: "andry ariadi blog about description",
+};
 
 export default function AboutPage() {
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.textContainer}>
-          <motion.h2 variants={fadeIn("up", 0.1)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }} className={styles.subTitle}>
-            About Agency
-          </motion.h2>
-          <motion.h1 variants={fadeIn("up", 0.5)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }} className={styles.title}>
-            We create digital ideas that are bigger, bolder, braver and better.
-          </motion.h1>
-          <motion.p variants={fadeIn("up", 0.7)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }} className={styles.desc}>
-            {" "}
-            We create digital ideas that are bigger, bolder, braver and better. We believe in good ideas flexibility and precission We’re world’s Our Special Team best consulting & finance solution provider. Wide range of web and software
-            development services.
-          </motion.p>
-          <motion.div variants={fadeIn("up", 0.9)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }}>
-            <Count />
-          </motion.div>
-        </div>
-        <motion.div variants={fadeIn("down", 0.3)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }} className={styles.imgContainer}>
-          <Image src="/images/about.svg" alt="about" fill className={styles.aboutImg} />
-        </motion.div>
-      </div>
+      <About />
     </>
   );
 }
---
+++ i-blog/src/components/about/About.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./about.module.css";
+import Count from "@/components/count/Count";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../libs/motions/variants";
+
+export default function About() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <motion.h2 variants={fadeIn("up", 0.1)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }} className={styles.subTitle}>
+            About Agency
+          </motion.h2>
+          <motion.h1 variants={fadeIn("up", 0.5)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }} className={styles.title}>
+            We create digital ideas that are bigger, bolder, braver and better.
+          </motion.h1>
+          <motion.p variants={fadeIn("up", 0.7)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }} className={styles.desc}>
+            {" "}
+            We create digital ideas that are bigger, bolder, braver and better. We believe in good ideas flexibility and precission We’re world’s Our Special Team best consulting & finance solution provider. Wide range of web and software
+            development services.
+          </motion.p>
+          <motion.div variants={fadeIn("up", 0.9)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }}>
+            <Count />
+          </motion.div>
+        </div>
+        <motion.div variants={fadeIn("down", 0.3)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.7 }} className={styles.imgContainer}>
+          <Image src="/images/about.svg" alt="about" fill className={styles.aboutImg} />
+        </motion.div>
+      </div>
+    </>
+  );
+}
