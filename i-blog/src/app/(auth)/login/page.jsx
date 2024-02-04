71b8d321a89df8d73323a28c4f3f740c6f41543f
Andry Ariadi <andryariadi23@gmail.com>
2024-02-04 16:33:21 +0700

    styling oauth btn

--- i-blog/src/app/(auth)/login/page.jsx
+++ i-blog/src/app/(auth)/login/page.jsx
@@ -2,6 +2,8 @@ import styles from "./login.module.css";
 import LoginForm from "@/components/loginForm/LoginForm";
 import { handleGithubLogin, handleGoogleLogin } from "@/libs/database/actions/action";
 import Image from "next/image";
+import { BsGithub } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
 
 export default function LoginPage() {
   return (
@@ -9,10 +11,20 @@ export default function LoginPage() {
       <div className={styles.container}>
         <div className={styles.formContainer}>
           <form action={handleGithubLogin}>
-            <button className={styles.oAuth}>GitHub</button>
+            <button className={styles.oAuth}>
+              <div>
+                <BsGithub size={30} />
+              </div>
+              <span>GitHub</span>
+            </button>
           </form>
           <form action={handleGoogleLogin}>
-            <button className={styles.oAuth}>Google</button>
+            <button className={styles.oAuth}>
+              <div>
+                <FcGoogle size={30} />
+              </div>
+              <span>Google</span>
+            </button>
           </form>
           <LoginForm />
         </div>
