7b91687a0df2501df4debcd03bcd79bc2ae4fecf
Andry Ariadi <andryariadi23@gmail.com>
2024-02-06 22:54:32 +0700

    add route api blog

---
+++ i-blog/src/app/api/blog/route.js
@@ -0,0 +1,16 @@
+import { connectToDB } from "@/libs/database";
+import Post from "@/libs/database/models/post.model";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+    connectToDB();
+
+    const posts = await Post.find().sort({ createdAt: -1 });
+
+    return NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch posts!");
+  }
+};
--- i-blog/src/app/blog/page.jsx
+++ i-blog/src/app/blog/page.jsx
@@ -7,15 +7,31 @@ export const metadata = {
   description: "andry ariadi blog post blog description",
 };
 
-export default async function BlogPage() {
-  const posts = await getPosts();
-  const users = await getUsers();
+const getAllPosts = async () => {
+  const res = await fetch("http://localhost:3000/api/blog", {
+    next: {
+      revalidate: 3600,
+    },
+  });
 
-  // console.log(posts, users, "<----diblogpage");
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  return res.json();
+};
+
+export default async function BlogPage() {
+  // Fetch data without an API
+  // const posts = await getPosts();
+  // const users = await getUsers();
+
+  // Fetch data with an API
+  const posts = await getAllPosts();
+
+  console.log(posts, "<----diblogpage");
   return (
     <>
       <div className={styles.container}>
-        {posts?.getAllPosts.map((post) => (
+        {posts?.map((post) => (
           <div className={styles.cardContainer} key={post._id}>
             <PostCard post={post} />
           </div>
